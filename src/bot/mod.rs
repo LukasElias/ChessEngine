16c6ab4c24f9d9663c134113971ba18865ec6003
Lukas Elias Lund Majland <lukaslundmajland@gmail.com>
2025-08-26 19:16:58 +0200

    Idk why I even had a lib.rs file, since I had a dependency on a library that did just that. Well, now I refactored it all, and is ready to make the actual logic

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -1,34 +1,44 @@
 use {
-    chess::*,
+    chess::{
+        Game,
+        Board,
+        Color,
+        ChessMove,
+        Error,
+    },
     std::time::Duration,
 };
 
 #[derive(Clone, Copy, Debug)]
 pub struct GameOptions {
-    time: Option<Duration>,
+    time: Duration,
     engine_plays: Color,
-}
-
-#[derive(Clone, Copy, Debug, Default)]
-pub struct Game {
-    board: Board,
-    game_options: GameOptions,
 }
 
 impl Default for GameOptions {
     fn default() -> Self {
         Self {
-            time: None,
+            time: Duration::from_secs(5 * 60), // 5 Minutes
             engine_plays: Color::Black,
         }
     }
 }
 
-impl Game {
-    fn new(game_options: GameOptions) -> Self {
+#[derive(Clone, Debug)]
+pub struct EngineMatch {
+    options: GameOptions,
+    game: Game,
+}
+
+impl EngineMatch {
+    pub fn new(options: GameOptions) -> Self {
         Self {
-            board: Board::default(),
-            game_options,
+            options,
+            game: Game::new(),
         }
+    }
+
+    pub fn user_move(chess_move: ChessMove) -> Result<(), Error> {
+        Ok(())
     }
 }
