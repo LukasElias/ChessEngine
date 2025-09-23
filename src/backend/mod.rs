1870d524a4d95b2b3e2d7bee434d511dfc30433f
Lukas Elias Lund Majland <lukaslundmajland@gmail.com>
2025-09-23 18:25:03 +0200

    I don't need to make a whole webserver, what am I doing?

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,44 +1,8 @@
-use {
-    crate::{
-        Game,
-        Player,
-    },
-    axum::{
-        extract::State,
-        response::{Html, IntoResponse},
-        routing::get,
-        Router,
-    },
-    std::sync::Arc,
-    tokio::sync::Mutex,
-};
-
-pub async fn run_server<W, B>(game: Arc<Mutex<Game<W, B>>>)
-where W: Player, B: Player {
-    let app = Router::new()
-        .route("/debug", get(serve_game_debug))
-        .route("/", get(serve_root))
-        .with_state(game);
-
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:6464")
-        .await
-        .unwrap();
-
-    axum::serve(listener, app)
-        .await
-        .unwrap();
-}
-
-async fn serve_game_debug<W, B>(State(game): State<Arc<Mutex<Game<W, B>>>>) -> impl IntoResponse
-where W: Player, B: Player {
-    let game_value = game.lock().await;
-
-    format!("{:#?}", game_value)
-}
-
-async fn serve_root<W, B>(State(game): State<Arc<Mutex<Game<W, B>>>>) -> Html<String>
-where W: Player, B: Player {
-    let game_value = game.lock().await;
-
-    Html(format!("{:#?}", game_value.current_board))
-}
+// use {
+//     crate::{
+//         Game,
+//         Player,
+//     },
+//     std::sync::Arc,
+//     tokio::sync::Mutex,
+// };
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-mod backend;
+// mod backend;
 mod bot;
 
 use {
-    std::sync::Arc,
-    tokio::sync::Mutex,
+    std::io::{
+        stdin,
+        stdout,
+        Write,
+        BufRead,
+        Result,
+    },
     bot::{
         Game,
         Human,
@@ -12,15 +17,33 @@ use {
     },
 };
 
-#[tokio::main]
-async fn main() {
-    let white = Human::default();
-    let black = Bot::default();
+fn main() -> Result<()> {
+    let stdin = stdin();
+    let mut stdout = stdout();
+    
+    for line in stdin.lock().lines() {
+        let cmd = line.unwrap(); // each line is a String without trailing newline
 
-    let game = Arc::new(Mutex::new(Game::new(white, black)));
+        match cmd.as_str() {
+            "uci" => {
+                writeln!(stdout, "id name ChessEngine")?;
+                writeln!(stdout, "id author Lukas Elias Lund Majland")?;
+                writeln!(stdout, "uciok")?;
+                stdout.flush()?;
+            }
+            "isready" => {
+            }
+            "quit" => break,
+            _ => { }
+        }
 
-    tokio::spawn(async move { backend::run_server(game.clone()).await });
-
-    loop {
     }
+
+    Ok(())
+
+
+    // let white = Human::default();
+    // let black = Bot::default();
+    //
+    // let game = Game::new(white, black);
 }
