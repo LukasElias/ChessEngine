cc5af01f96e1ce047b0de62aaf9dec2731ba1828
Lukas Elias Lund Majland <lukaslundmajland@gmail.com>
2025-10-04 13:19:44 +0200

    Now the position UCI command works with moves too and fen and everything ☕

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -2,6 +2,8 @@ use {
     chess::{
         Board,
         ChessMove,
+        Square,
+        Piece,
     },
     std::{
         fmt::Debug,
@@ -37,6 +39,9 @@ impl Engine {
             let line = line.unwrap();
             let mut parts = line.split_whitespace();
 
+            // TODO: Split this up into multiple functions
+            // TODO: Write info all the time to make sure how the engine is doing
+
             match parts.next() {
                 Some("uci") => {
                     writeln!(stdout, "id name ChessEngine")?;
@@ -64,13 +69,10 @@ impl Engine {
                         } else if position_op == Some("fen") {
                             let mut fen = String::new();
 
-                            for i in 0..6 {
-                                println!("{}", i);
+                            for _ in 0..6 {
                                 fen.push_str(parts.next().expect("This is an invalid fen string. The program will now panic. TODO: Don't panic here 👍"));
                                 fen.push(' ');
                             }
-
-                            println!("fen: {}", fen);
 
                             self.game = Some(Game {
                                 current_board: Board::from_str(fen.as_str()).unwrap(),
@@ -78,11 +80,38 @@ impl Engine {
                             })
                         }
 
-                        // Apply the moves if any
+                        if let Some(game) = &mut self.game {
+                            if parts.next() == Some("moves") {
+                                while let Some(move_notation) = parts.next() {
+                                    let src_square = Square::from_str(&move_notation[0..2]).expect("You gave the wrong input here dummy");
+                                    let dest_square = Square::from_str(&move_notation[2..4]).expect("Same on this line dummy");
+                                    let promotion = match move_notation.chars().nth(4) {
+                                        Some('q') => Some(Piece::Queen),
+                                        Some('n') => Some(Piece::Knight),
+                                        Some('r') => Some(Piece::Rook),
+                                        Some('b') => Some(Piece::Bishop),
+                                        Some(_) => None,
+                                        None => None,
+                                    };
+                                    
+                                    let chess_move = ChessMove::new(src_square, dest_square, promotion);
+
+                                    game.current_board = game.current_board.make_move_new(chess_move);
+
+                                    game.moves.push(chess_move);
+                                }
+                            }
+                        }
                     }
                 },
                 Some("quit") => break,
                 _ => { },
+            }
+
+            // debug
+            if let Some(game) = &self.game {
+                writeln!(stdout, "info string board {} moves {:?}", game.current_board, game.moves)?;
+                stdout.flush()?;
             }
         }
 
